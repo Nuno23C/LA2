ab6ee6ff52c1f16b5feab929a8b29efe09d54ab0
Nuno Costa <75766357+Nuno23C@users.noreply.github.com>
2022-03-06 15:04:28 +0000

    Add files via upload

---
+++ Treino 1/aloca.py
@@ -0,0 +1,33 @@
+'''
+Implemente uma função que dado um dicionário com as preferências dos alunos
+por projectos (para cada aluno uma lista de identificadores de projecto,
+por ordem de preferência), aloca esses alunos aos projectos. A alocação
+é feita por ordem de número de aluno, e cada projecto só pode ser feito
+por um aluno. A função deve devolver a lista com os alunos que não ficaram
+alocados a nenhum projecto, ordenada por ordem de número de aluno.
+'''
+
+def aloca(prefs):
+    # prefs = {10885: [1,5], 40000: [5], 10000: [1,2]}
+    projects = {}
+    alunoNaoAlocs = []
+
+    new_prefs = []
+    for aluno in prefs:
+        new_prefs.append((aluno,prefs[aluno]))
+
+    new_prefs.sort(key = lambda x : (x[0]))
+
+    # new_prefs = [(10000, [1, 2]), (10885, [1, 5]), (40000, [5])]
+
+    for aluno in new_prefs:
+        projects_alocated = projects.values()
+        for project in aluno[1]:
+            if project not in projects_alocated and aluno[0] not in projects:
+                projects[aluno[0]] = project
+        if aluno[0] not in projects:
+            alunoNaoAlocs.append(aluno[0])
+
+    print(projects)
+
+    return alunoNaoAlocs
